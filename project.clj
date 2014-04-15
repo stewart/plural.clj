db9ed970603f5910bbd4306353810b3df5734981
Andrew Stewart <andrew@stwrt.ca>
2014-04-14 21:25:16 -0700

    Lock version and add url + description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject plural "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject plural "0.1.0"
+  :description "Small util to pluralize words"
+  :url "https://github.com/stewart/plural.clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
